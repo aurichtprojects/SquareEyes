8ea9190caf8e0b4ea0be53b99ffd89ca793b7525
Herve Senot <hsenot@gmail.com>
2013-05-31 12:02:17 +1000

    Added non-verbose schema export

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,6 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.1.4
--- Dumped by pg_dump version 9.1.4
--- Started on 2013-05-24 10:32:26
-
 SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
@@ -17,21 +13,17 @@ SET search_path = public, pg_catalog;
 SET default_with_oids = false;
 
 --
--- TOC entry 169 (class 1259 OID 17228)
--- Dependencies: 5
 -- Name: asset; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE asset (
     gid smallint NOT NULL,
-    code character varying(5),
-    label character varying(32)
+    code character varying(3),
+    label character varying(64)
 );
 
 
 --
--- TOC entry 168 (class 1259 OID 17226)
--- Dependencies: 5 169
 -- Name: asset_gid_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -44,8 +36,6 @@ CREATE SEQUENCE asset_gid_seq
 
 
 --
--- TOC entry 2536 (class 0 OID 0)
--- Dependencies: 168
 -- Name: asset_gid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -53,8 +43,6 @@ ALTER SEQUENCE asset_gid_seq OWNED BY asset.gid;
 
 
 --
--- TOC entry 167 (class 1259 OID 17217)
--- Dependencies: 5 1031
 -- Name: cell; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -65,44 +53,17 @@ CREATE TABLE cell (
 
 
 --
--- TOC entry 166 (class 1259 OID 17215)
--- Dependencies: 167 5
--- Name: cell_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE cell_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- TOC entry 2537 (class 0 OID 0)
--- Dependencies: 166
--- Name: cell_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE cell_id_seq OWNED BY cell.id;
-
-
---
--- TOC entry 179 (class 1259 OID 25531)
--- Dependencies: 5
 -- Name: current_occurence; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE current_occurence (
-    asset_code character varying(5),
+    asset_code character varying(3),
     cell_id smallint,
     status smallint
 );
 
 
 --
--- TOC entry 171 (class 1259 OID 17238)
--- Dependencies: 5
 -- Name: observation; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -119,8 +80,6 @@ CREATE TABLE observation (
 
 
 --
--- TOC entry 178 (class 1259 OID 17333)
--- Dependencies: 5
 -- Name: observation_coverage; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -132,8 +91,6 @@ CREATE TABLE observation_coverage (
 
 
 --
--- TOC entry 177 (class 1259 OID 17331)
--- Dependencies: 5 178
 -- Name: observation_coverage_gid_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -146,8 +103,6 @@ CREATE SEQUENCE observation_coverage_gid_seq
 
 
 --
--- TOC entry 2538 (class 0 OID 0)
--- Dependencies: 177
 -- Name: observation_coverage_gid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -155,8 +110,6 @@ ALTER SEQUENCE observation_coverage_gid_seq OWNED BY observation_coverage.gid;
 
 
 --
--- TOC entry 170 (class 1259 OID 17236)
--- Dependencies: 5 171
 -- Name: observation_gid_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -169,8 +122,6 @@ CREATE SEQUENCE observation_gid_seq
 
 
 --
--- TOC entry 2539 (class 0 OID 0)
--- Dependencies: 170
 -- Name: observation_gid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -178,8 +129,6 @@ ALTER SEQUENCE observation_gid_seq OWNED BY observation.gid;
 
 
 --
--- TOC entry 176 (class 1259 OID 17265)
--- Dependencies: 5
 -- Name: r_role; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -190,8 +139,6 @@ CREATE TABLE r_role (
 
 
 --
--- TOC entry 173 (class 1259 OID 17249)
--- Dependencies: 5
 -- Name: r_status; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -202,8 +149,6 @@ CREATE TABLE r_status (
 
 
 --
--- TOC entry 172 (class 1259 OID 17247)
--- Dependencies: 5 173
 -- Name: r_status_gid_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -216,8 +161,6 @@ CREATE SEQUENCE r_status_gid_seq
 
 
 --
--- TOC entry 2540 (class 0 OID 0)
--- Dependencies: 172
 -- Name: r_status_gid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -225,8 +168,6 @@ ALTER SEQUENCE r_status_gid_seq OWNED BY r_status.gid;
 
 
 --
--- TOC entry 175 (class 1259 OID 17257)
--- Dependencies: 5
 -- Name: user; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -238,8 +179,6 @@ CREATE TABLE "user" (
 
 
 --
--- TOC entry 174 (class 1259 OID 17255)
--- Dependencies: 5 175
 -- Name: user_gid_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -252,8 +191,6 @@ CREATE SEQUENCE user_gid_seq
 
 
 --
--- TOC entry 2541 (class 0 OID 0)
--- Dependencies: 174
 -- Name: user_gid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -261,8 +198,6 @@ ALTER SEQUENCE user_gid_seq OWNED BY "user".gid;
 
 
 --
--- TOC entry 2509 (class 2604 OID 25438)
--- Dependencies: 169 168 169
 -- Name: gid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -270,17 +205,6 @@ ALTER TABLE ONLY asset ALTER COLUMN gid SET DEFAULT nextval('asset_gid_seq'::reg
 
 
 --
--- TOC entry 2508 (class 2604 OID 25422)
--- Dependencies: 166 167 167
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY cell ALTER COLUMN id SET DEFAULT nextval('cell_id_seq'::regclass);
-
-
---
--- TOC entry 2510 (class 2604 OID 25446)
--- Dependencies: 170 171 171
 -- Name: gid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -288,8 +212,6 @@ ALTER TABLE ONLY observation ALTER COLUMN gid SET DEFAULT nextval('observation_g
 
 
 --
--- TOC entry 2513 (class 2604 OID 25477)
--- Dependencies: 178 177 178
 -- Name: gid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -297,8 +219,6 @@ ALTER TABLE ONLY observation_coverage ALTER COLUMN gid SET DEFAULT nextval('obse
 
 
 --
--- TOC entry 2511 (class 2604 OID 25499)
--- Dependencies: 173 172 173
 -- Name: gid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -306,8 +226,6 @@ ALTER TABLE ONLY r_status ALTER COLUMN gid SET DEFAULT nextval('r_status_gid_seq
 
 
 --
--- TOC entry 2512 (class 2604 OID 25506)
--- Dependencies: 174 175 175
 -- Name: gid; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -315,8 +233,6 @@ ALTER TABLE ONLY "user" ALTER COLUMN gid SET DEFAULT nextval('user_gid_seq'::reg
 
 
 --
--- TOC entry 2518 (class 2606 OID 17235)
--- Dependencies: 169 169
 -- Name: asset_code_uk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -325,8 +241,6 @@ ALTER TABLE ONLY asset
 
 
 --
--- TOC entry 2520 (class 2606 OID 25440)
--- Dependencies: 169 169
 -- Name: asset_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -335,8 +249,6 @@ ALTER TABLE ONLY asset
 
 
 --
--- TOC entry 2516 (class 2606 OID 25424)
--- Dependencies: 167 167
 -- Name: cell_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -345,8 +257,6 @@ ALTER TABLE ONLY cell
 
 
 --
--- TOC entry 2532 (class 2606 OID 25479)
--- Dependencies: 178 178
 -- Name: observation_coverage_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -355,8 +265,6 @@ ALTER TABLE ONLY observation_coverage
 
 
 --
--- TOC entry 2522 (class 2606 OID 25448)
--- Dependencies: 171 171
 -- Name: observation_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -365,8 +273,6 @@ ALTER TABLE ONLY observation
 
 
 --
--- TOC entry 2530 (class 2606 OID 25494)
--- Dependencies: 176 176
 -- Name: role_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -375,8 +281,6 @@ ALTER TABLE ONLY r_role
 
 
 --
--- TOC entry 2524 (class 2606 OID 25501)
--- Dependencies: 173 173
 -- Name: status_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -385,8 +289,6 @@ ALTER TABLE ONLY r_status
 
 
 --
--- TOC entry 2526 (class 2606 OID 17264)
--- Dependencies: 175 175
 -- Name: user_name_uk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -395,8 +297,6 @@ ALTER TABLE ONLY "user"
 
 
 --
--- TOC entry 2528 (class 2606 OID 25508)
--- Dependencies: 175 175
 -- Name: user_pk; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -405,17 +305,13 @@ ALTER TABLE ONLY "user"
 
 
 --
--- TOC entry 2514 (class 1259 OID 17328)
--- Dependencies: 167 1936
--- Name: cell_geom_idx; Type: INDEX; Schema: public; Owner: -
+-- Name: cell_the_geom_idx; Type: INDEX; Schema: public; Owner: -
 --
 
-CREATE INDEX cell_geom_idx ON cell USING gist (the_geom);
+CREATE INDEX cell_the_geom_idx ON cell USING gist (the_geom);
 
 
 --
--- TOC entry 2533 (class 1259 OID 25534)
--- Dependencies: 179
 -- Name: co_asset_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -423,8 +319,6 @@ CREATE INDEX co_asset_idx ON current_occurence USING btree (asset_code);
 
 ALTER TABLE current_occurence CLUSTER ON co_asset_idx;
 
-
--- Completed on 2013-05-24 10:32:27
 
 --
 -- PostgreSQL database dump complete
