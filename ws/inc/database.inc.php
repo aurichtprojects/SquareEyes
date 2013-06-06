623e1930f14f2efeead33d041b8444cee6882661
Herve Senot <hsenot@gmail.com>
2013-06-06 12:17:44 +1000

    Added PHP web service to retrieve asset list on the fly

---
+++ ws/inc/database.inc.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Database Include
+ * Handles all database functions required by the REST web services.
+ */
+
+/**
+ * Return postgres data connection
+ * @return 		object		- adodb data connection
+ */
+function pgConnection() {
+	try {
+		// Connect to the database passed in the config variable
+		$conn = new PDO ("pgsql:host=localhost;dbname=squareeyes;port=54321","<USERNAME>","<PASSWORD>", array(PDO::ATTR_PERSISTENT => true));
+	    return $conn;
+	}
+	catch (Exception $e) {
+		trigger_error("Caught Exception: " . $e->getMessage(), E_USER_ERROR);
+	}
+}
+
+?>
---
+++ ws/v3/ws_asset_list.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Returns a list of assets from the database as JSON file
+ */
+
+# Includes
+require_once("../inc/error.inc.php");
+require_once("../inc/database.inc.php");
+require_once("../inc/security.inc.php");
+
+# Set arguments for error email
+$err_user_name = "Herve";
+$err_email = "herve.senot@groundtruth.com.au";
+
+# Performs the query and returns XML or JSON
+try {
+	$format = 'json';
+	$sql = sanitizeSQL("SELECT id,label FROM asset");
+	$pgconn = pgConnection();
+
+    /*** fetch into an PDOStatement object ***/
+    $recordSet = $pgconn->prepare($sql);
+    $recordSet->execute();
+
+	if ($format == 'json') {
+		require_once("../inc/json.pdo.inc.php");
+		header("Content-Type: application/json");
+		echo rs2json($recordSet);
+	}
+	elseif ($format == "text") {
+		header("Content-Type: application/text");
+		while($line = $recordSet->fetch(PDO::FETCH_ASSOC))
+		{
+			foreach ($line as $col_key => $col_val)
+			{
+				echo $col_val . "\n";
+			}
+		}
+	}
+	else {
+		trigger_error("Caught Exception: format must be json or text.", E_USER_ERROR);
+	}
+}
+catch (Exception $e) {
+	trigger_error("Caught Exception: " . $e->getMessage(), E_USER_ERROR);
+}
+
+?>
