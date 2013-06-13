bb22a02fc34ba7353000cb0fc277d8e3a1f0bd21
Herve Senot <hsenot@gmail.com>
2013-06-13 17:43:21 +1000

    Managing file upload from the form: upload, link from DB, info message

--- ws/ws_create_observation.php
+++ ws/ws_create_observation.php
@@ -39,15 +39,68 @@ try {
     $recordSet = $pgconn->prepare($sql);
     $recordSet->execute();
 
+    // Getting the observation number just attributed (somehow curr_val does not always work)
+	$sql = "SELECT last_value from observation_gid_seq;";
+	$sql = sanitizeSQL($sql);
+	//echo $sql;
+    $recordSet = $pgconn->prepare($sql);
+    $recordSet->execute();
+
+	while ($row  = $recordSet->fetch())
+	{
+		$obs_id = $row[0];
+	}
+
 	// Another block to insert the observation coverage
-	$sql2 = "INSERT INTO observation_coverage(cell_id,observation_id) SELECT c.id,currval('observation_gid_seq') FROM (SELECT id FROM cell WHERE id IN (".$p_cells_id.")) c;";
+	$sql2 = "INSERT INTO observation_coverage(cell_id,observation_id) SELECT c.id,".$obs_id." FROM (SELECT id FROM cell WHERE id IN (".$p_cells_id.")) c;";
 	$sql2 = sanitizeSQL($sql2);
 	//echo $sql2;
     $recordSet2 = $pgconn->prepare($sql2);
     $recordSet2->execute();
 
 	header("Content-Type: application/json");
-	echo fs2json($recordSet2);
+	$uploaded_img_webpath = "NONE";
+
+    // Now processing the uploaded file
+	if ($_FILES["field_file"]["error"] > 0)
+    {
+	    exit('{"success":"false","error":{"code":"'.$_FILES["file"]["error"].'"}');
+    }
+    else
+    {
+		// Moving the file to the uploads directory
+		$upload_path = "../uploads/";
+		$ftyp = $_FILES["field_file"]["type"];
+		if ($ftyp == "image/gif")
+		{$fext = "gif";}
+		elseif (($ftyp == "image/jpeg") || ($ftyp == "image/jpg")|| ($ftyp == "image/pjpeg"))
+		{$fext = "jpg";}
+		elseif (($ftyp == "image/x-png") || ($ftyp == "image/png"))
+		{$fext = "png";}
+		else
+		{exit('{"success":"false","error":{"code":"U","message":"Unknown mime type:'.$ftyp.'"}');}
+
+		// Building the filename out of the observation number and the mime type
+		$target_path = $upload_path . $obs_id .".". $fext;
+
+		if(move_uploaded_file($_FILES['field_file']['tmp_name'], $target_path)) {
+			//echo "The file ".  basename( $_FILES['uploadedfile']['name'])." has been uploaded";
+		} else {
+			exit('{"success":"false","error":{"code":"U","message":"The file upload to '.$target_path.' did not complete succeessfully"}}');
+		}
+
+		// Image web path:
+		$uploaded_img_webpath = "http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/".$target_path;
+		// Inserting the observation
+		$sql = "UPDATE observation SET photo='".$uploaded_img_webpath."' WHERE id=".$obs_id;
+		$sql = sanitizeSQL($sql);
+		//echo $sql;
+	    $recordSet = $pgconn->prepare($sql);
+	    $recordSet->execute();
+
+    }
+
+	echo '{"success":"true","observation_id":"'.$obs_id.'","uploaded_img":"'.$uploaded_img_webpath.'"}';
 }
 catch (Exception $e) {
 	trigger_error("Caught Exception: " . $e->getMessage(), E_USER_ERROR);
