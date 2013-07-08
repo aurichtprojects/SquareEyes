b17e7e7e27a70eda78baf6d3f7945819b07504c5
Herve Senot <hsenot@gmail.com>
2013-07-08 16:14:35 +1000

    Exporting in the correct directory

--- db/export/full-export.bat
+++ db/export/full-export.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\pgAdmin III\1.14\pg_dump.exe" --host localhost --port 54321 --username "postgres" --no-password  --format plain --no-owner --encoding UTF8 --no-privileges --no-tablespaces --no-unlogged-table-data --file "C:\Program Files (x86)\OpenGeo\OpenGeo Suite\webapps\SquareEyes\db\full-export.sql" --schema=nz "squareeyes"
+"C:\Program Files (x86)\pgAdmin III\1.14\pg_dump.exe" --host localhost --port 54321 --username "postgres" --no-password  --format plain --no-owner --encoding UTF8 --no-privileges --no-tablespaces --no-unlogged-table-data --file "C:\Program Files (x86)\OpenGeo\OpenGeo Suite\webapps\SquareEyes\db\export\full-export.sql" --schema=nz "squareeyes"
 
 pause
